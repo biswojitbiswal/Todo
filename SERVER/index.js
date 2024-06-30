1fdcf7f524e0175c855504b267642e5fb388a2ea
Biswojit Biswal <biswojitb474@gmail.com>
2024-06-30 23:28:43 +0530

    fix some error

--- SERVER/index.js
+++ SERVER/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-    origin: ["https://todo-nonh1sa73-biswojit.vercel.app"],
+    origin: process.env.CORS_ORIGIN,
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true
 }))
