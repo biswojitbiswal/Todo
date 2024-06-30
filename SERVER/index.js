10fc2e4439e34388cb4b40daa12923cdd4908c2b
Biswojit Biswal <biswojitb474@gmail.com>
2024-06-30 23:24:56 +0530

    add url on cors

--- SERVER/index.js
+++ SERVER/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ["https://https://todo-nonh1sa73-biswojit.vercel.app/"],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true
 }))
