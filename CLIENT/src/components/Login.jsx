4404c1f75be762c94d648cf1c73c470b6999df54
Biswojit Biswal <biswojitb474@gmail.com>
2024-07-03 00:29:31 +0530

    fix some error

--- CLIENT/src/components/Login.jsx
+++ CLIENT/src/components/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch(`https://todo-nine-bice-68.vercel.app/api/todo/users/login`, {
+      const response = await fetch(`https://todo-server-liard-omega.vercel.app/api/todo/users/login`, {
         method: "POST",
         headers: {
           'Content-Type' : 'application/json'
