2905961f733978414845df9d52e9caad77ca1256
Biswojit Biswal <biswojitb474@gmail.com>
2024-07-03 23:35:36 +0530

    some changes

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -13,23 +13,23 @@ import DeleteLink from './components/DeleteLink'
 import UpdateImage from './components/UpdateImage'
 
 function App() {
-     const router = createBrowserRouter(
-       createRoutesFromElements(
-         <Route path='' element={<Layout />}>
-           <Route path='/' element={<Todos />} />
-           <Route path='/login' element={<Login />} />
-           <Route path='/register' element={<Register />} /> 
-           <Route path='/contact' element={<Contact />} />
-           <Route path="/user" element={<User />} />
-             <Route path="/user/passreset" element={<PassChange />} />
-             <Route path="/user/deleteuser/:id" element={<DeleteLink />} />
-             <Route path='/user/updateimage/:id' element={<UpdateImage />} />
-           <Route path='/logout' element={<Logout />} />
-           <Route path='*' element={<Error />} />
-         </Route>
+    //  const router = createBrowserRouter(
+    //    createRoutesFromElements(
+    //      <Route path='' element={<Layout />}>
+    //        <Route path='/' element={<Todos />} />
+    //        <Route path='/login' element={<Login />} />
+    //        <Route path='/register' element={<Register />} /> 
+    //        <Route path='/contact' element={<Contact />} />
+    //        <Route path="/user" element={<User />} />
+    //          <Route path="/user/passreset" element={<PassChange />} />
+    //          <Route path="/user/deleteuser/:id" element={<DeleteLink />} />
+    //          <Route path='/user/updateimage/:id' element={<UpdateImage />} />
+    //        <Route path='/logout' element={<Logout />} />
+    //        <Route path='*' element={<Error />} />
+    //      </Route>
       
-       )
-     )
+    //    )
+    //  )
   return (
     <>
       <Router>
