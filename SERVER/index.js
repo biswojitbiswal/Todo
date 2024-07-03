f27ee2142577a8edad183e71ea80df26d120f017
Biswojit Biswal <biswojitb474@gmail.com>
2024-07-03 11:00:15 +0530

    add backend url

--- SERVER/index.js
+++ SERVER/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://todo-gamma-ecru.vercel.app",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true
 }))
