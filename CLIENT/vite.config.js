39f75db71e5e35de7accb0b82c28abc927072e47
Biswojit Biswal <biswojitb474@gmail.com>
2024-07-03 12:08:45 +0530

    fix error

--- CLIENT/vite.config.js
+++ CLIENT/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Adjust this according to your deployment URL if not hosted at the root
   plugins: [react()],
 })
