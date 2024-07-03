efa55e3dfb7557b98b50b6ac05c309cec0ee96fb
Biswojit Biswal <biswojitb474@gmail.com>
2024-07-03 23:30:15 +0530

    some changes

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import Todos from './components/Todos'
 import Layout from './components/Layout'
 import Register from './components/Register'
@@ -12,33 +12,42 @@ import PassChange from './components/PassChange'
 import DeleteLink from './components/DeleteLink'
 import UpdateImage from './components/UpdateImage'
 
-
-
-
 function App() {
-
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='' element={<Layout />}>
-        <Route path='/' element={<Todos />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} /> 
-        <Route path='/contact' element={<Contact />} />
-        <Route path="/user" element={<User />} />
-          <Route path="/user/passreset" element={<PassChange />} />
-          <Route path="/user/deleteuser/:id" element={<DeleteLink />} />
-          <Route path='/user/updateimage/:id' element={<UpdateImage />} />
-        <Route path='/logout' element={<Logout />} />
-        <Route path='*' element={<Error />} />
-      </Route>
+     const router = createBrowserRouter(
+       createRoutesFromElements(
+         <Route path='' element={<Layout />}>
+           <Route path='/' element={<Todos />} />
+           <Route path='/login' element={<Login />} />
+           <Route path='/register' element={<Register />} /> 
+           <Route path='/contact' element={<Contact />} />
+           <Route path="/user" element={<User />} />
+             <Route path="/user/passreset" element={<PassChange />} />
+             <Route path="/user/deleteuser/:id" element={<DeleteLink />} />
+             <Route path='/user/updateimage/:id' element={<UpdateImage />} />
+           <Route path='/logout' element={<Logout />} />
+           <Route path='*' element={<Error />} />
+         </Route>
       
-    )
-  )
-
+       )
+     )
   return (
     <>
-     <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path='' element={<Layout />}>
+           <Route path='/' element={<Todos />} />
+           <Route path='/login' element={<Login />} />
+           <Route path='/register' element={<Register />} /> 
+           <Route path='/contact' element={<Contact />} />
+           <Route path="/user" element={<User />} />
+             <Route path="/user/passreset" element={<PassChange />} />
+             <Route path="/user/deleteuser/:id" element={<DeleteLink />} />
+             <Route path='/user/updateimage/:id' element={<UpdateImage />} />
+           <Route path='/logout' element={<Logout />} />
+           <Route path='*' element={<Error />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
