c76c7c97e0cacae8263e748a1e9e1bd85678e013
Biswojit Biswal <biswojitb474@gmail.com>
2024-07-03 10:34:19 +0530

    add frontend url

--- SERVER/index.js
+++ SERVER/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-    origin: ["https://todo-nine-bice-68.vercel.app"],
+    origin: ["https://todo-one-flame.vercel.app"],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true
 }))
