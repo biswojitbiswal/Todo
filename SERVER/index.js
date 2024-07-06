85f539aa4ce0ea165a1430e7b0e787bb29763804
Biswojit Biswal <biswojitb474@gmail.com>
2024-07-06 20:53:17 +0530

    test

--- SERVER/index.js
+++ SERVER/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-    origin: ["https://todo-iota-topaz.vercel.app"],
+    origin: process.env.CORS_ORIGIN,
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true
 }))
