24f2bd2e806ed8388cbfdc31100ecc8a404cab20
Biswojit Biswal <biswojitb474@gmail.com>
2024-07-06 22:58:58 +0530

    testing

--- SERVER/middlewares/multer.middlewares.js
+++ SERVER/middlewares/multer.middlewares.js
@@ -3,7 +3,7 @@
  
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      cb(null, path.join(__dirname, '../public/temp'))
     },
     filename: function (req, file, cb) {
       
