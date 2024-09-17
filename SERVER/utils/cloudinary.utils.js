b0b8dd37f804873a1da0d7c9e883a749e5a0be9f
Biswojit Biswal <biswojitb474@gmail.com>
2024-09-17 15:51:11 +0530

    changes on utils file

--- SERVER/utils/cloudinary.utils.js
+++ SERVER/utils/cloudinary.utils.js
@@ -1,89 +1,3 @@
-// // import {v2 as cloudinary} from 'cloudinary'
-// // import fs from 'fs'
-
-// // cloudinary.config({
-// //     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-// //     api_key: process.env.CLOUDINARY_API_KEY, 
-// //     api_secret: process.env.CLOUDINARY_API_SECRET
-// // })
-
-// // const uploadCloudinary = async(localFilePath) => {
-// //     try {
-// //         if(!localFilePath){
-// //             return null;
-// //         }
-
-// //         const response = await cloudinary.uploader.upload(localFilePath,{
-// //             resource_type: "auto"
-// //         })
-
-// //         fs.unlinkSync(localFilePath);
-// //         return response;
-
-// //     } catch (error) {
-// //         fs.unlinkSync(localFilePath)
-// //         return null;
-// //     }
-// // }
-
-// // export {uploadCloudinary}
-
-
-// import { v2 as cloudinary } from 'cloudinary';
-// import fs from 'fs';
-// import path from 'path';
-// import axios from 'axios';
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// const downloadImage = async (url, filepath) => {
-//   const writer = fs.createWriteStream(filepath);
-//   const response = await axios({
-//     url,
-//     method: 'GET',
-//     responseType: 'stream',
-//   });
-
-//   response.data.pipe(writer);
-
-//   return new Promise((resolve, reject) => {
-//     writer.on('finish', resolve);
-//     writer.on('error', reject);
-//   });
-// };
-
-// const uploadCloudinary = async (filePathOrUrl) => {
-//   let localFilePath = filePathOrUrl;
-//   const isUrl = filePathOrUrl.startsWith('http');
-
-//   if (isUrl) {
-//     localFilePath = path.join('public', 'temp', 'avatar.jpg');
-//     await downloadImage(filePathOrUrl, localFilePath);
-//   }
-
-//   try {
-//     const response = await cloudinary.uploader.upload(localFilePath, {
-//       resource_type: 'auto',
-//     });
-
-//     if (isUrl && fs.existsSync(localFilePath)) {
-//       fs.unlinkSync(localFilePath);
-//     }
-
-//     return response;
-//   } catch (error) {
-//     if (isUrl && fs.existsSync(localFilePath)) {
-//       fs.unlinkSync(localFilePath);
-//     }
-//     throw error;
-//   }
-// };
-
-// export { uploadCloudinary };
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
@@ -98,14 +12,12 @@ const uploadCloudinary = async (localFilePath) => {
         if(!localFilePath) {
             return null;
         }
-        // upload the file on cloudinary
+
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto",
         })
 
-        // file has been uploaded succesfully
-        // console.log("file is uploaded successfully", response);
-        // after upload unlink the file from server
+
         fs.unlinkSync(localFilePath) 
         return response;
     } catch (error) {
